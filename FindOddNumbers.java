6f1d9c79a5e461723f0db493ad6ba207de8033ba
seemajeyak <seemajeya.k@gmail.com>
2024-06-18 20:51:04 -0400

    Add files via upload
    Day2

---
+++ FindOddNumbers.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class FindOddNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int maxRange=10;
+int i;
+for(i=0;i<=maxRange;i++)
+
+if(i%2==1)
+{
+System.out.println(i);
+}
+
+}
+}
